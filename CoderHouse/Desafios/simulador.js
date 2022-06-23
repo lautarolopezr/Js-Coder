4fa92e474efaddd45353151847b7ab82df37913e
Lautaro Lopez Rodriguez <lautarolopezr@gmail.com>
2022-06-23 13:16:24 -0300

    CoderHouse/Desafios/simulador.js

--- CoderHouse/Desafios/simulador.js
+++ CoderHouse/Desafios/simulador.js
@@ -1,19 +1,3 @@
-/* 
->> Consigna: Con los conocimientos vistos hasta el momento, empezarás a armar la estructura inicial de tu proyecto integrador. A partir de los ejemplos mostrados la primera clase, deberás:
-Pensar el alcance de tu proyecto: ¿usarás un cotizador de seguros? ¿un simulador de créditos? ¿un simulador personalizado?
-Armar la estructura HTML del proyecto.
-Incorporar lo ejercitado en las clases anteriores, algoritmo condicional y algoritmo con ciclo.
-Utilizar funciones para realizar esas operaciones.
->>Aspectos a incluir en el entregable:
-Archivo HTML y Archivo JS, referenciado en el HTML por etiqueta <script src="js/miarchivo.js"></script>, que incluya la definición de un algoritmo en JavaScript que emplee funciones para resolver el procesamiento principal del simulador
->>Ejemplo:
-Calcular costo total de productos y/o servicios seleccionados por el usuario.
-Calcular pagos en cuotas sobre un monto determinado.
-Calcular valor final de un producto seleccionado en función de impuestos y descuentos.
-Calcular tiempo de espera promedio en relación a la cantidad de turnos registrados.
-Calcular edad promedio de personas registradas.
-Calcular nota final de alumnos ingresados.
-*/
 
 
 // const cuotas = (producto = prompt("Ingrese el producto")) =>{
@@ -79,7 +63,7 @@ const cuotasv2 = (n = prompt("Ingrese la cantidad de productos a ingresar")) =>{
             console.log(impuesto)    
             total = precio + impuesto
             total = total.toFixed(2)
-            console.info(`El precio de ${producto} es de $${precio} pesos y en cuotas de ${cuotas} y con impuesto de ${impuesto12}% queda en ${total}`)
+            console.info(`El precio de ${producto} es de $${precio} pesos y en cuotas de $${cuotas} y con impuesto de ${impuesto12}% queda en ${total}`)
             break;
     
             case 18:
